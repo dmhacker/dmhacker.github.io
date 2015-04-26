acfde5bb350b6c14f710a53d7eea9d02cfb1dc93
David <dmhacker@yahoo.com>
2015-04-25 19:34:50 -0700

    Cleanup code

--- js/secure-redirect.js
+++
@@ -1,5 +0,0 @@
-var host = "dmhacker.github.io";
-var page_protocol = "https:";
-
-if ((host == window.location.host) && (window.location.protocol != page_protocol))
-    window.location.protocol = page_protocol;
