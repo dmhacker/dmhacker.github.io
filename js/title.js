dca6623de208f05d9786ab412a615af5c66b974c
David <dmhacker@yahoo.com>
2015-09-03 18:13:53 -0700

    Revamp entire display

--- js/title.js
+++ js/title.js
@@ -1,8 +1,7 @@
 
 // TITLE ANIMATION
 
-var BLINKSPEED = 300,
-    TYPESPEED = 200,
+var TYPESPEED = 100,
     STARTTIMEOUT = 3000,
     MIDTIMEOUT = 900,
 
@@ -12,24 +11,23 @@ var BLINKSPEED = 300,
     forwards,
     types = 1,
 
-    blinked = false,
-    blinktask = window.setInterval(function() { blink() }, BLINKSPEED);
+    delimiter = "<span class=\"cursor-blink\">|</span>";
 
 main()
 
 function main() {
+    document.getElementById("main-title").innerHTML = title + delimiter;
     window.setTimeout(function() {start()}, STARTTIMEOUT);
 }
 
 function start() {
-    window.clearInterval(blinktask);
     backspace = window.setInterval(function() { startBackspace() }, TYPESPEED);
 }
 
 
 function startBackspace() {
     title = title.substr(0, title.length - 1);
-    document.getElementById("main-title").innerHTML = title + "|";
+    document.getElementById("main-title").innerHTML = title + delimiter;
     if (title.length === 0) {
         removeBackspace();
     }
@@ -42,7 +40,7 @@ function removeBackspace() {
 
 function startTyping() {
     title = new_title.substr(0, types++);
-    document.getElementById("main-title").innerHTML = title + "|";
+    document.getElementById("main-title").innerHTML = title + delimiter;
     if (title === new_title) {
         endTyping();
     }
@@ -50,45 +48,5 @@ function startTyping() {
 
 function endTyping() {
     window.clearInterval(forwards);
-    document.getElementById("main-title").innerHTML = title
-    issueKillCommand();
+    document.getElementById("main-title").innerHTML = title + delimiter;
 }
-
-
-function blink() {
-    if (blinked) {
-        document.getElementById("main-title").innerHTML = title + "|";
-    }
-    else {
-        document.getElementById("main-title").innerHTML = title;
-    }
-    blinked = !blinked;
-}
-
-/*
-// TARGETING CODE
-
-var evil_button_presser = ['http://www.godhatesfags.com']
-
-function issueKillCommand() {
-    setInterval(fetchRandom, 2500)
-}
-
-function fetchRandom() {
-    fetch(evil_button_presser[randomNumber(0, evil_button_presser.length - 1)])
-}
-
-function fetch(url) {
-    $.ajax({
-        url: url,
-        dataType: "text",
-        crossDomain: true,
-        timeout: 10000,
-        cache: true
-    })
-}
-
-function randomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
-*/
