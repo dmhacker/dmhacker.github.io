754c2c5dc6c3b01c31096c392738f6e8703c8458
David Hacker <dmhacker@yahoo.com>
2016-01-22 11:53:15 -0800

    Update copyright

--- static/js/copyright-updater.js
+++ static/js/copyright-updater.js
@@ -5,7 +5,7 @@ function main() {
     var d = new Date();
     var year = d.getFullYear();
     var copyright = document.getElementById(element_name);
-    copyright.innerHTML = "Copyright &copy; "+year+" &mdash; David Hacker";
+    copyright.innerHTML = "&copy; "+year+" David Hacker &mdash; All Rights Reserved";
 }
 
 main();
