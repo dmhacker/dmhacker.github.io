33dff4366dea8bfb015bcb7da57367bfdcd07c58
David Hacker <dmhacker@protonmail.com>
2019-12-04 10:04:00 -0800

    Add katex to get latex support

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,17 @@ module.exports = {
         anonymize: false,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-katex`,
+          options: {
+            // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+            strict: `ignore`
+          }
+        }],
+      },
+    },
   ],
 }
